683715a520b22526843cfa1670ee99bbf0165f95
mytrop98bussines-stack <mytrop98bussines@gmail.com>
2026-05-11 23:19:10 -0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAxUOLLm72in2-OM30Ywrxq9-QBuYdH334",
+  authDomain: "cupcoin-b2b4f.firebaseapp.com",
+  projectId: "cupcoin-b2b4f",
+  storageBucket: "cupcoin-b2b4f.firebasestorage.app",
+  messagingSenderId: "502564207166",
+  appId: "1:502564207166:web:ef81ebbebf6e390142ef6f",
+  measurementId: "G-EGNMNRTPND"
 };
-
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
